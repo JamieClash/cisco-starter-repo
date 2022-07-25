fcb8b25292175cca8a89324f5b1fc4e1b3a9fa66
Jamie Yeung <jamiethy@gmail.com>
2022-07-25 09:30:14 +0100

    Test

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { getQueriesForElement } from '@testing-library/react';
+import React from 'react';
 import './App.css';
 
 function App() {
+  class Banner extends React.Component{
+
+    render(){
+      return(
+        <div className = "banner">
+        <title>
+          {this.props.title}
+        </title>
+        </div>
+      );
+    }
+  }
+
+  class Exhibit extends React.Component{
+
+    render(){
+      return(
+        <div className = "exhibit">
+          <div className = "exhibit__heading">
+            <h1>{this.props.heading}</h1>
+            <div className = "exhibit__body">
+              {this.props.children}
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Banner title="Sextant"/>
+      <div className = "wrapper">
+        <Exhibit heading="Example exhibit">
+          <p>This is an example exhibit card.</p>
+        </Exhibit>
+      </div>
     </div>
   );
 }
